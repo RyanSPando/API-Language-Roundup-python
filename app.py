beaaa0b68c9dd29730431ec094b6443138f2d45c
Ryan "archaen" Pando <vretha@gmail.com>
2016-11-10 12:19:17 -0700

    Added pg2 db and donuts route

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+import os
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
+url = urlparse.urlparse(os.environ.get('DATABASE_URL'))
+db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
+schema = "schema.sql"
+conn = psycopg2.connect(db)
+cur = conn.cursor()
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@app.route('/donuts')
+def donuts():
+    try:
+        cur.execute("""SELECT * from donut""")
+        rows = cur.fetchall()
+        response = ''
+        my_list = []
+        for row in rows:
+            my_list.append(row[0])
+
+        return jsonify(my_list)
+    except Exception as e:
+        print e
+        return []
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
